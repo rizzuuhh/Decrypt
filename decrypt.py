fad81ad36960bd502b2387a3b1a0dc0a7fd16462
rizza <denostarizza43@gmail.com>
2023-04-18 17:20:37 +1200

    all characters are listed

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,6 @@
 # list all the charscters
+characters = "*&#+!"
+key = "aeiou"
 # * changed to a, & changed to e, # changed to i, + changed to o, ! changed to u,
 # ask user's encrypted text
 # print ouptput
