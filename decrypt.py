d30e2dc8c50a1e76132343d57514b2c8b66bd698
rizza <denostarizza43@gmail.com>
2023-04-18 21:11:05 +1200

    ask user's input

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,8 @@
 # list all the charscters
 characters = "*&#+!"
 key = "aeiou"
-# * changed to a, & changed to e, # changed to i, + changed to o, ! changed to u,
 # ask user's encrypted text
+text = input("Enter your encrypted text: ")
+
+# * changed to a, & changed to e, # changed to i, + changed to o, ! changed to u,
 # print ouptput
