1812ee759ef93eb95267e3675a15ac8acaa2bfd6
rizza <denostarizza43@gmail.com>
2023-04-18 23:37:22 +1200

    change *&#+! to aeiou

--- decrypt.py
+++ decrypt.py
@@ -3,6 +3,12 @@ characters = "*&#+!"
 key = "aeiou"
 # ask user's encrypted text
 text = input("Enter your encrypted text: ")
-
 # * changed to a, & changed to e, # changed to i, + changed to o, ! changed to u,
+decrypt_text = ""
+for letter in text:
+    if letter.lower() in characters:
+        decrypt_text += key[characters.find(letter.lower())]
+    else:
+        decrypt_text += letter
 # print ouptput
+
